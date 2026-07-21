257a385277f7a6a976656032896dc0bb8766130e
DevBlzAlphaK1dXD <doanvien909@gmail.com>
2026-07-21 23:04:29 +0700

    Add cookie and storage management APIs to preload script

--- preload.js
+++ preload.js
@@ -5,4 +5,12 @@ contextBridge.exposeInMainWorld("electronAPI", {
   minimizeWindow: () => ipcRenderer.send("win:minimize"),
   toggleMaximizeWindow: () => ipcRenderer.send("win:toggle-maximize"),
   openExternal: (url) => ipcRenderer.send("open-external", url),
+  
+  // Cookie management
+  getCookies: (url) => ipcRenderer.invoke("get-cookies", url),
+  setCookie: (cookie) => ipcRenderer.invoke("set-cookie", cookie),
+  clearCookies: () => ipcRenderer.invoke("clear-cookies"),
+  
+  // Storage management
+  getStorageInfo: () => ipcRenderer.invoke("get-storage-info"),
 });
